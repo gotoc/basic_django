6ca6ff35c2ccc211a12e4d33d5d861f49e650bf7
Jeffrey Wong <jvwong@jvwong-desktop.(none)>
2014-03-14 00:12:35 -0400

    changed db permissions

--- config/settings/jraywo.py
+++ config/settings/jraywo.py
@@ -2,7 +2,7 @@ import os
 
 APP_NAME = "jraywo"
 
-DEBUG = False
+DEBUG = True
 
 #Pre-configured paths
 SETTINGS_DIR = os.path.abspath(os.path.dirname(__file__)) 
@@ -37,7 +37,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*',]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
