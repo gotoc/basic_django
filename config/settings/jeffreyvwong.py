e38bc8e866cda637f690111e8069a5ec4dd6f170
Jeffrey Wong <jvwong@jvw-server.(none)>
2014-03-14 16:26:24 -0400

    latest update

--- config/settings/jeffreyvwong.py
+++ config/settings/jeffreyvwong.py
@@ -37,7 +37,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['107.170.63.152']
+ALLOWED_HOSTS = ['.jeffreyvwong.ca','107.170.63.152']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
