efeb17e90fcb809910afe5601c8d632cf0970ab1
Jeffrey Wong <jvwong@outlook.com>
2014-03-14 10:40:05 -0400

    latest update

--- fabfile.py
+++ fabfile.py
@@ -2,7 +2,7 @@ from __future__ import with_statement
 from fabric.api import *
 
 def commit():
-    with settings(warn_only=True):
+    with settings(warn_only=False):
         local('git commit -a -m "latest update"')
 
 def push():
