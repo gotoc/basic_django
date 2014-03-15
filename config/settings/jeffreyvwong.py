19cf430129bffaefcb1cd5df4d4324b0cc99444e
Jeffrey Wong <jvwong@jvw-server.(none)>
2014-03-15 14:07:22 -0400

    readded settings on server

--- config/settings/jeffreyvwong.py
+++ config/settings/jeffreyvwong.py
@@ -7,7 +7,8 @@ DEBUG = False
 #Pre-configured paths
 SETTINGS_DIR = os.path.abspath(os.path.dirname(__file__)) 
 CONFIG_DIR = os.path.abspath(os.path.join(SETTINGS_DIR, "..")) 
-PARENT_DIR = os.path.abspath(os.path.join(CONFIG_DIR, "..")) 
+PARENT_DIR = os.path.abspath(os.path.join(CONFIG_DIR, ".."))
+STATIC_PATH = os.path.abspath(os.path.join(PARENT_DIR, "static"))
 
 DATABASE = ".".join([APP_NAME, "db"])
 DATABASE_NAME = os.path.abspath(os.path.join(PARENT_DIR, "sites", APP_NAME, DATABASE))
@@ -16,7 +17,7 @@ TEMPLATE_PATH = os.path.abspath(os.path.join(PARENT_DIR, "templates"))
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Jeffrey Wong', 'jvwong@outlook.com'),
 )
 
 MANAGERS = ADMINS
@@ -36,7 +37,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.jeffreyvwong.ca','107.170.63.152']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -63,7 +64,7 @@ USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.abspath(os.path.join(PARENT_DIR, "static/jelly/images/"))
+MEDIA_ROOT = '/var/www/jeffreyvwong/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -74,7 +75,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/static/'
+STATIC_ROOT = '/var/www/jeffreyvwong/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -85,6 +86,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    STATIC_PATH,
 )
 
 # List of finder classes that know how to find static files in
@@ -140,11 +142,8 @@ INSTALLED_APPS = (
     'django.contrib.comments',
     'django.contrib.markup',
     'tagging',
-    'south',
     'json_field',
-    'jelly'
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'jeffreyvwong'
 )
 
 
@@ -177,3 +176,4 @@ LOGGING = {
         },
     }
 }
+
