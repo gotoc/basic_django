a2e88c9747e0cebd1ca99114c7b61b42d672f988
Jeffrey Wong <jvwong@outlook.com>
2014-03-15 19:00:56 -0400

    update tons

---
+++ apache_conf/django_projects/jeffreyvwong.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import site
+
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir('/home/jvwong/.virtualenvs/practical_django/local/lib/python2.7/site-packages')
+
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/home/jvwong/projects/django_projects')
+sys.path.append('/home/jvwong/projects/django_projects/config')
+sys.path.append('/home/jvwong/.virtualenvs/practical_django')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.jeffreyvwong'
+
+# Activate your virtual env
+activate_env=os.path.expanduser("/home/jvwong/.virtualenvs/practical_django/bin/activate_this.py")
+execfile(activate_env, dict(__file__=activate_env))
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
